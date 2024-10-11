1483e8fb6e0db2ecd1044407911e3b78f025779d
yashelter <yashelter@yandex.ru>
2024-10-11 08:04:46 +0300

    added image downloader, fixed scaling for images, started requests logic

---
+++ Assets/Scripts/Connections/ImageDownloader.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Connections
+{
+    public static class ImageDownloader
+    {
+        public static async Task DownloadAndSaveImageAsync(string url, string savePath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var imageData = await client.GetByteArrayAsync(url);
+                    await File.WriteAllBytesAsync(savePath, imageData);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError("Error while saving or downloading: " + e.Message);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Connections;
 using UnityEngine;
 using XML;
 
 public sealed class GameManager : MonoBehaviour
 {
     private static XModule xModule;
+    private static ConnectionsModule _connectionsModule = new ConnectionsModule();
     private List<IXHandler> handlers = new List<IXHandler>();
+
+    [SerializeField] public static readonly string OutputPath = Application.dataPath + "/Out";
+    [SerializeField] public static readonly string InputPath =  Application.dataPath + "/Input"; // Only no server logic
 
     private void Awake()
     {
@@ -46,5 +52,11 @@ public sealed class GameManager : MonoBehaviour
             handler.CallBack();
         }
         xModule.LogDocument();
+    }
+
+    public void TestImage(string url = @"https://avatars.mds.yandex.net/i?id=9a7f15bfd1db79112f1fd527886da06e_l-4255244-images-thumbs&n=13")
+    {
+        Debug.Log(Application.dataPath);
+        _connectionsModule.DownloadImage(url, Application.dataPath + "/Out" + "/test.png");
     }
 }
