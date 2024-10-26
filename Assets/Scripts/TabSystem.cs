ac1ddc6e22de32a10040fa1d8a1d0a7e3f18bc72
yashelter <yashelter@yandex.ru>
2024-10-27 01:08:06 +0300

    made tab system

---
+++ Assets/Scripts/TabSystem.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable]
+public class TabController
+{
+    public Button trigger;
+    public GameObject panel;
+}
+
+/// <summary>
+/// 
+/// </summary>
+public class TabSystem : MonoBehaviour
+{
+    private TabController activeTab;
+    public List<TabController> tabs = new List<TabController>();
+    
+    private void Start()
+    {
+        if (tabs.Count == 0)
+        {
+            Debug.LogWarning("No Tabs Available, check inspector");
+            Debug.LogWarning("For save usage component self destroys");
+            Destroy(this);
+            return;
+        }
+        gameObject.transform.SetSiblingIndex(0);
+        
+        foreach (var tab in tabs)
+        {
+            if (tab.trigger == null || tab.panel == null)
+            {
+                Debug.LogWarning("Tab was null, check inspector");
+                continue;
+            }
+            tab.panel.SetActive(false);
+            tab.trigger.onClick.AddListener(() => ActivateTab(tab));
+        }
+        
+        activeTab = tabs[0];
+        activeTab.panel.SetActive(true);
+    }
+
+    private void ActivateTab(TabController tab)
+    {
+        activeTab.panel.SetActive(false);
+        
+        activeTab = tab;
+        activeTab.panel.SetActive(true);
+    }
+
+}
